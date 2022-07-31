7bc8c8e53c55c4bab35ad2fcff7ae5ba17bcf014
unknown <lorena.vasconcellos2003@gmail.com>
2022-07-31 12:58:25 -0300

    Versão final

---
+++ script.js
@@ -0,0 +1,39 @@
+function showviolao () {
+  document.getElementById('hideviolao').setAttribute('id', 'qualquer');
+}
+
+function hideviolao() {
+  document.getElementById('qualquer').setAttribute('id', 'hideviolao');
+}
+
+function showpercussao () {
+  document.getElementById('hidepercussao').setAttribute('id', 'qualquer');
+}
+
+function hidepercussao() {
+  document.getElementById('qualquer').setAttribute('id', 'hidepercussao');
+}
+
+function showguitarra () {
+  document.getElementById('hideguitarra').setAttribute('id', 'qualquer');
+}
+
+function hideguitarra() {
+  document.getElementById('qualquer').setAttribute('id', 'hideguitarra');
+}
+
+function showcanto() {
+  document.getElementById('hidecanto').setAttribute('id', 'qualquer');
+}
+
+function hidecanto() {
+  document.getElementById('qualquer').setAttribute('id', 'hidecanto');
+}
+
+function showcontrabaixo() {
+  document.getElementById('hidecontrabaixo').setAttribute('id', 'qualquer');
+}
+
+function hidecontrabaixo() {
+  document.getElementById('qualquer').setAttribute('id', 'hidecontrabaixo');
+}
